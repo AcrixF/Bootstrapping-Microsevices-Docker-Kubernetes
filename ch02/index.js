5b12827eec0c2d894c875c198cfa573be009956c
Cristian Flores Martínez <acrixf@gmail.com>
2021-03-14 13:45:57 -0600

    Add video stream enpoint

--- ch02/index.js
+++ ch02/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 
@@ -11,3 +12,19 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`);
 });
+
+app.get("/video", ((req, res) => {
+    const path = '/Users/acrixf/Desktop/big_buck_bunny_720p_1mb.mp4';
+    fs.stat(path, ((err, stats) => {
+        if (err) {
+            console.error("An error occurred");
+            res.sendStatus(500);
+            return;
+        }
+        res.writeHead(200, {
+            "Content-Length": stats.size,
+            "Content-Type": "video/mp4",
+        });
+        fs.createReadStream(path).pipe(res);
+    }))
+}))
