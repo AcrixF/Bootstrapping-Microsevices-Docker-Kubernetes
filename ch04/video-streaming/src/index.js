10ce979c4ec3a1736d113aefde4b2168bfa50592
Cristian Flores Martínez <acrixf@gmail.com>
2021-03-22 19:07:22 -0600

    update microservices

--- ch04/video-streaming/src/index.js
+++ ch04/video-streaming/src/index.js
@@ -1,31 +1,44 @@
 const express = require('express');
-const fs = require('fs');
+const http = require("http");
 
 const app = express();
 
+if (!process.env.PORT) {
+    throw new Error("Please specify the port number for the HTTP server with the environment variable PORT.");
+}
 
-const PORT = 3000;
+if (!process.env.VIDEO_STORAGE_HOST) {
+    throw new Error("Please specify the host name for the video storage microservice in variable VIDEO_STORAGE_HOST.");
+}
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+if (!process.env.VIDEO_STORAGE_PORT) {
+    throw new Error("Please specify the port number for the video storage microservice in variable VIDEO_STORAGE_PORT.");
+}
+
+const PORT = process.env.PORT;
+const VIDEO_STORAGE_HOST = process.env.VIDEO_STORAGE_HOST;
+const VIDEO_STORAGE_PORT = parseInt(process.env.VIDEO_STORAGE_PORT);
+
+console.log(`Forwarding video requests to ${VIDEO_STORAGE_HOST}:${VIDEO_STORAGE_PORT}.`);
 
 app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}!`);
+    console.log('Video Streaming Microservice Online.');
 });
 
-app.get("/video", ((req, res) => {
-    const path = './videos/big_buck_bunny_720p_1mb.mp4';
-    fs.stat(path, ((err, stats) => {
-        if (err) {
-            console.error("An error occurred");
-            res.sendStatus(500);
-            return;
+app.get("/video", (req, res) => {
+    const forwardRequest = http.request( // Forward the request to the video storage microservice.
+        {
+            host: VIDEO_STORAGE_HOST,
+            port: VIDEO_STORAGE_PORT,
+            path: '/video?path=big_buck_bunny_720p_1mb.mp4',
+            method: 'GET',
+            headers: req.headers
+        },
+        forwardResponse => {
+            res.writeHead(forwardResponse.statusCode, forwardResponse.headers);
+            forwardResponse.pipe(res);
         }
-        res.writeHead(200, {
-            "Content-Length": stats.size,
-            "Content-Type": "video/mp4",
-        });
-        fs.createReadStream(path).pipe(res);
-    }))
-}))
+    );
+
+    req.pipe(forwardRequest);
+});
