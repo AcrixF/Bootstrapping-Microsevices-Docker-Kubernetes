9f177bf0c5bfcb7f21c4a6613728ab656be1f7d7
Cristian Flores Martínez <acrixf@gmail.com>
2021-03-24 23:45:06 -0600

    Docker HotReloading Tool

---
+++ ch05/video-streaming/src/index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const fs = require("fs");
+
+function setupHandlers(app) {
+    app.get("/video", (req, res) => {
+        const videoPath = "./videos/SampleVideo_1280x720_1mb.mp4";
+        fs.stat(videoPath, (err,stats) => {
+            if (err) {
+                console.error("An error occurred");
+                res.sendStatus(500);
+                return;
+            }
+
+            res.writeHead(200, {
+                "Content-Length": stats.size,
+                "Content-Type": "video/mp4",
+            });
+
+            fs.createReadStream(videoPath).pipe(res);
+        })
+    })
+}
+
+function startHttpServer() {
+    return new Promise((resolve, reject) => {
+        const app = express();
+        setupHandlers(app);
+
+        const port = process.env.PORT && parseInt(process.env.PORT) || 3000;
+        app.listen(port, () => resolve());
+    });
+}
+
+function main() {
+    return startHttpServer();
+}
+
+main()
+    .then(() => console.log("Video Streaming Microservice online."))
+    .catch(err => {
+        console.error("Video Streaming Microservice failed to start.");
+        console.error(err && err.stack || err);
+    });
