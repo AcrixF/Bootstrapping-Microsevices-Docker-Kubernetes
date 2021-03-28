fea97e4817bf17803de5f0907f3724ce648ead21
Cristian Flores Martínez <acrixf@gmail.com>
2021-03-28 11:15:50 -0600

    Direct Messaging between Microservices

--- ch05/video-streaming/src/index.js
+++ ch05/video-streaming/src/index.js
@@ -1,5 +1,32 @@
 const express = require("express");
 const fs = require("fs");
+const http = require("http");
+
+function sendViewedMessage(videoPath) {
+    const postOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    };
+
+    const requestBody = {
+        videoPath: videoPath
+    }
+
+    const req = http.request("http://history/viewed", postOptions);
+
+    req.on("close", () => {
+        console.log("Sent 'viewed' message to history microservice.")
+    });
+    req.on("error", (err) => {
+        console.error("Failed to send 'viewed' message!");
+        console.error(err && err.stack || err);
+    });
+
+    req.write(JSON.stringify(requestBody));
+    req.end();
+}
 
 function setupHandlers(app) {
     app.get("/video", (req, res) => {
@@ -17,6 +44,7 @@ function setupHandlers(app) {
             });
 
             fs.createReadStream(videoPath).pipe(res);
+            sendViewedMessage(videoPath); // Send message to history microservice
         })
     })
 }
